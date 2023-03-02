2204cd0da183f872fc30284629b4fde620d26ec3
Alexander <klivets.alex@gmail.com>
2023-03-02 10:40:22 +0300

    feat: add modal

--- source/js/modules/more-text.js
+++ source/js/modules/more-text.js
@@ -1,9 +1,11 @@
 const buttonMore = document.querySelector('.about__more');
 const descriptionText = document.querySelector('.about__description--more');
+const textWrapper = document.querySelector('.about__text-wrapper');
 
 const showMoreText = () => {
   buttonMore.addEventListener('click', () => {
     descriptionText.classList.toggle('about__description-active');
+    textWrapper.classList.toggle('about__text-wrapper--active');
     if (buttonMore.textContent === 'Подробнее') {
       buttonMore.textContent = 'свернуть';
     } else {
